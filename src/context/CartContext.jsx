79c1f4184092c4ffd3c5d4b945da3a834b45562f
Jose Rangel <rangelreyesjose.jarr@gmail.com>
2026-04-13 13:34:52 -0700

    fix: Fix format issues

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -9,12 +9,12 @@ export const CartProvider = ({ children }) => {
   const [cart, setCart] = useState([]);
 
   const addToCart = (product) => {
-    alert('¡Producto agregado al carrito!')
+    alert('¡Producto agregado al carrito!');
     setCart([...cart, product]);
   };
 
   const clearCart = () => {
-    alert('¡Carrito vaciado!')
+    alert('¡Carrito vaciado!');
     setCart([]);
   };
 
